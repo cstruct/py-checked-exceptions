0162168213301049ec07860c03f7e7fd95336979
Albin Skott <albin@mattsson.io>
2025-08-16 12:54:39 +0200

    Fix CLI argument docs

--- src/args.rs
+++ src/args.rs
@@ -24,21 +24,17 @@ pub(crate) struct CheckCommand {
     /// Run the command within the given project directory.
     ///
     /// All `pyproject.toml` files will be discovered by walking up the directory tree from the given project directory,
-    /// as will the project's virtual environment (`.venv`) unless the `venv-path` option is set.
+    /// as will the project's virtual environment (`.venv`).
     ///
     /// Other command-line arguments (such as relative paths) will be resolved relative to the current working directory.
     #[arg(long, value_name = "PROJECT")]
     pub(crate) project: Option<SystemPathBuf>,
 
-    /// Run the command within the given project directory.
-    ///
-    /// All `pyproject.toml` files will be discovered by walking up the directory tree from the given project directory,
-    /// as will the project's virtual environment (`.venv`) unless the `venv-path` option is set.
-    ///
-    /// Other command-line arguments (such as relative paths) will be resolved relative to the current working directory.
+    /// Filter what files to check.
     #[arg(long, value_name = "FILTER")]
     pub(crate) filter: Option<SystemPathBuf>,
 
+    /// Set base exceptions to target when analyzing.
     #[arg(long, value_name = "FILTER")]
     pub(crate) target_exceptions: Vec<String>,
 }
